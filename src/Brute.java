d656b397c1b7c18a3f1ef59c0b6ce7abc3d5d3dc
Alexey Kutepov <alkutepov@gmail.com>
2015-02-10 12:55:54 +0400

    Bug fix in Brute

--- src/Brute.java
+++ src/Brute.java
@@ -11,19 +11,19 @@ public class Brute {
       y = in.readInt();
       pointList[i] = new Point(x, y);
     }
-
+    Merge.sort(pointList);
     for (int i = 0; i < pointList.length; i++) {
-      for (int j = 0; j < pointList.length; j++) {
+      for (int j = i + 1; j < pointList.length; j++) {
         if (pointList[i].compareTo(pointList[j]) == 0) {
           continue;
         }
-        for (int k = 0; k < pointList.length; k++) {
+        for (int k = j + 1; k < pointList.length; k++) {
           if (pointList[i].compareTo(pointList[k]) == 0 || pointList[j].compareTo(pointList[k]) == 0) {
             continue;
           } else if (pointList[i].slopeTo(pointList[j]) !=  pointList[i].slopeTo(pointList[k])) {
             continue;
           }
-          for (int t = 0; t < pointList.length; t++) {
+          for (int t = k + 1; t < pointList.length; t++) {
             if (pointList[i].compareTo(pointList[t]) == 0 || pointList[j].compareTo(pointList[t]) == 0 || pointList[k].compareTo(pointList[t]) == 0) {
               continue;
             } else if (pointList[i].slopeTo(pointList[k]) ==  pointList[i].slopeTo(pointList[t])) {
