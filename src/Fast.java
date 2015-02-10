189d233e29fc160add6e18eb5fac89fe60d7fcd0
Alexey Kutepov <alkutepov@gmail.com>
2015-02-10 17:25:52 +0400

    Style fix

--- src/Fast.java
+++ src/Fast.java
@@ -20,14 +20,14 @@ public class Fast {
       pointListNotSorted[i] = new Point(x, y);
       pointList[i].draw();
     }
-    Merge.sort(pointListNotSorted);
+
     for (int i = 0; i < pointListNotSorted.length; i++) {
       if (hashSet.contains(pointListNotSorted[i].toString())) {
         continue;
       }
       Arrays.sort(pointList, pointListNotSorted[i].SLOPE_ORDER);
-      int index = pointList.length - 1, count = 1;
-      for (int j = pointList.length - 2; j >= 0; j--) {
+      int index = 0, count = 1;
+      for (int j = 1; j < pointList.length; j++) {
         if (pointListNotSorted[i].SLOPE_ORDER.compare(pointList[index], pointList[j]) == 0) {
           count++;
         } else {
@@ -35,12 +35,12 @@ public class Fast {
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.append(pointListNotSorted[i].toString());
             hashSet.add(pointListNotSorted[i].toString());
-            for (int k = index; k > index-count; k--) {
+            for (int k = index; k > index+count; k++) {
               stringBuilder.append(" -> " + pointList[k].toString());
               hashSet.add(pointList[k].toString());
             }
             StdOut.println(stringBuilder.toString());
-            pointListNotSorted[i].drawTo(pointList[index-count+1]);
+            pointListNotSorted[i].drawTo(pointList[index+count-1]);
           }
           count = 1;
           index = j;
