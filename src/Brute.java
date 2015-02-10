30dcb3b33d110c80aaec09b9ab6bfabbf3173345
Alexey Kutepov <alkutepov@gmail.com>
2015-02-10 12:05:23 +0400

    Created Brute class

--- src/Brute.java
+++ src/Brute.java
@@ -3,6 +3,35 @@
  */
 public class Brute {
   public static void main(String[] args) {
+    In in = new In(args[0]);
+    Point[] pointList = new Point[in.readInt()];
+    int x, y;
+    for (int i = 0; i < pointList.length; i++) {
+      x = in.readInt();
+      y = in.readInt();
+      pointList[i] = new Point(x, y);
+    }
 
+    for (int i = 0; i < pointList.length; i++) {
+      for (int j = 0; j < pointList.length; j++) {
+        if (pointList[i].compareTo(pointList[j]) == 0) {
+          continue;
+        }
+        for (int k = 0; k < pointList.length; k++) {
+          if (pointList[i].compareTo(pointList[k]) == 0 || pointList[j].compareTo(pointList[k]) == 0) {
+            continue;
+          } else if (pointList[i].slopeTo(pointList[j]) !=  pointList[i].slopeTo(pointList[k])) {
+            continue;
+          }
+          for (int t = 0; t < pointList.length; t++) {
+            if (pointList[i].compareTo(pointList[t]) == 0 || pointList[j].compareTo(pointList[t]) == 0 || pointList[k].compareTo(pointList[t]) == 0) {
+              continue;
+            } else if (pointList[i].slopeTo(pointList[k]) ==  pointList[i].slopeTo(pointList[t])) {
+              StdOut.println(pointList[i].toString() + " -> " + pointList[j].toString() + " -> " + pointList[k].toString() + " -> " + pointList[t].toString());
+            }
+          }
+        }
+      }
+    }
   }
 }
