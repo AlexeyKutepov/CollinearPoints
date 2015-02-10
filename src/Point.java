848d9b3d42cb241052a75a3716d4ff573eddb37b
Alexey Kutepov <alkutepov@gmail.com>
2015-02-10 11:34:44 +0400

    Created Point class

--- src/Point.java
+++ src/Point.java
@@ -3,33 +3,44 @@ import java.util.Comparator;
 public class Point implements Comparable<Point> {
 
   // compare points by slope
-  public final Comparator<Point> SLOPE_ORDER = null;
+  public final Comparator<Point> SLOPE_ORDER = new SlopeOrder();
 
   private final int x;                              // x coordinate
   private final int y;                              // y coordinate
 
+  private class SlopeOrder implements Comparator<Point> {
+    public int compare(Point q1, Point q2) {
+      double slope1 = slopeTo(q1);
+      double slope2 = slopeTo(q2);
+      if (slope1 < slope2) {
+        return -1;
+      } else if (slope1 > slope2) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  }
+
   // create the point (x, y)
   public Point(int x, int y) {
-        /* DO NOT MODIFY */
     this.x = x;
     this.y = y;
   }
 
   // plot this point to standard drawing
   public void draw() {
-        /* DO NOT MODIFY */
     StdDraw.point(x, y);
   }
 
   // draw line between this point and that point to standard drawing
   public void drawTo(Point that) {
-        /* DO NOT MODIFY */
     StdDraw.line(this.x, this.y, that.x, that.y);
   }
 
   // slope between this point and that point
   public double slopeTo(Point that) {
-    return (that.y - this.y)/(that.x - this.x);
+    return ((double)that.y - (double)this.y)/((double)that.x - (double)this.x);
   }
 
   // is this point lexicographically smaller than that one?
@@ -46,7 +57,6 @@ public class Point implements Comparable<Point> {
 
   // return string representation of this point
   public String toString() {
-        /* DO NOT MODIFY */
     return "(" + x + ", " + y + ")";
   }
 
