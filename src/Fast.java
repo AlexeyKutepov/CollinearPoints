5c836fcee31c2a0ed49ea2f072d7ba4f3521f2cf
Alexey Kutepov <alkutepov@gmail.com>
2015-02-11 12:26:58 +0400

    Raw Score	100.00 / 100.00

--- src/Fast.java
+++ src/Fast.java
@@ -9,7 +9,7 @@ import java.util.HashSet;
 public class Fast {
 
   public static void main(String[] args) {
-    HashSet<String> hashSet = new HashSet<String>();
+    HashSet<String> hashSetLine = new HashSet<String>();
     StdDraw.setXscale(0, 32768);
     StdDraw.setYscale(0, 32768);
     In in = new In(args[0]);
@@ -25,9 +25,6 @@ public class Fast {
     }
     ArrayList<Point> arrayList = new ArrayList<Point>();
     for (int i = 0; i < pointListNotSorted.length; i++) {
-      if (hashSet.contains(pointListNotSorted[i].toString())) {
-        continue;
-      }
       Arrays.sort(pointList, pointListNotSorted[i].SLOPE_ORDER);
       int index = 0, count = 1;
       for (int j = 1; j < pointList.length; j++) {
@@ -36,7 +33,7 @@ public class Fast {
           arrayList.add(pointList[j]);
         } else {
           if (count >= 3) {
-            solve(arrayList, hashSet, pointListNotSorted[i]);
+            solve(arrayList, hashSetLine, pointListNotSorted[i]);
           }
           arrayList.clear();
           arrayList.add(pointList[j]);
@@ -45,25 +42,25 @@ public class Fast {
         }
       }
       if (count >= 3) {
-        solve(arrayList, hashSet, pointListNotSorted[i]);
+        solve(arrayList, hashSetLine, pointListNotSorted[i]);
       }
       arrayList.clear();
     }
   }
 
-  private static void solve(ArrayList<Point> arrayList, HashSet<String> hashSet, Point mainPoint) {
+  private static void solve(ArrayList<Point> arrayList, HashSet<String> hashSetLine, Point mainPoint) {
     arrayList.add(mainPoint);
     Collections.sort(arrayList);
-
-    hashSet.add(arrayList.get(0).toString());
 
     StringBuilder stringBuilder = new StringBuilder();
     stringBuilder.append(arrayList.get(0).toString());
     for (int k = 1; k < arrayList.size(); k++) {
       stringBuilder.append(" -> " + arrayList.get(k).toString());
-      hashSet.add(arrayList.get(k).toString());
     }
-    StdOut.println(stringBuilder.toString());
-    arrayList.get(0).drawTo(arrayList.get(arrayList.size()-1));
+    if (!hashSetLine.contains(stringBuilder.toString())) {
+      hashSetLine.add(stringBuilder.toString());
+      StdOut.println(stringBuilder.toString());
+      arrayList.get(0).drawTo(arrayList.get(arrayList.size() - 1));
+    }
   }
 }
